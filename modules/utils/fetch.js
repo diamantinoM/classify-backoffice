64f78d048806a9e5b7fad15f682e48a0e1a670e9
wnicolass <nicolaswgl99@gmail.com>
2023-05-07 20:25:04 +0100

    feat: create some fetch functions

--- modules/utils/fetch.js
+++ modules/utils/fetch.js
@@ -2,6 +2,29 @@ import { showErrorMessage } from "./message-box.js";
 
 const token = window.sessionStorage.getItem("token");
 
+async function validateResponse(response) {
+  if (!response.ok) {
+    return await showErrorMessage();
+  }
+  return response;
+}
+
+export async function postWithAuth(url, body) {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...body }),
+    });
+    return validateResponse(response);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 export async function getWithAuth(url) {
   try {
     const response = await fetch(url, {
@@ -10,27 +33,7 @@ export async function getWithAuth(url) {
         authorization: `Bearer ${token}`,
       },
     });
-    if (!response.ok) {
-      return await showErrorMessage();
-    }
-    return response;
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-export async function deleteWithAuth(url) {
-  try {
-    const response = await fetch(url, {
-      method: "DELETE",
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
-    });
-    if (!response.ok) {
-      return await showErrorMessage();
-    }
-    return response;
+    return validateResponse(response);
   } catch (err) {
     console.error(err);
   }
@@ -46,10 +49,21 @@ export async function updateWithAuth(url, method, body) {
       },
       body: JSON.stringify({ ...body }),
     });
-    if (!response.ok) {
-      return await showErrorMessage();
-    }
-    return response;
+    return validateResponse(response);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function deleteWithAuth(url) {
+  try {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+    return validateResponse(response);
   } catch (err) {
     console.error(err);
   }
