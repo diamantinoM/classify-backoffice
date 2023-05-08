3f4c97056348be451e91e56d9d79a7750b47264c
Diamantino <filipemoura869@gmail.com>
2023-05-08 12:36:05 +0100

    feat: implement saving window position

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const path = require('path');
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { dialog } = require('electron');
-const { getWindowSettings, saveBounds } = require('./settings')
+const { getWindowSettings, saveBounds, savePosition, getWindowPosition } = require('./settings')
 
 const isMac = process.platform === 'darwin';
 
 function createMainWindow () {
     const bounds = getWindowSettings();
+    const position = getWindowPosition();
     
     // Create the browser window.
     const mainWindow = new BrowserWindow({
         title: 'Classify App',
         width:  bounds[0],
         height: bounds[1],
+        x: position[0],
+        y: position[1],
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         },
@@ -21,6 +24,7 @@ function createMainWindow () {
     ipcMain.handle('dialog', (_, options) => dialog.showMessageBox(options));
     ipcMain.handle('setToken', (_, token) => Store.setItem('token', token));
     
+    mainWindow.on('moved', () => savePosition(mainWindow.getPosition()));
     mainWindow.on('resized', () => saveBounds(mainWindow.getSize()));
     
     // and load the index.html of the app.
