263010e24cfafd088be67a72686d517cf1091c91
Diamantino <filipemoura869@gmail.com>
2023-05-08 12:25:24 +0100

    feat: implement saving window size

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 const path = require('path');
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { dialog } = require('electron');
+const { getWindowSettings, saveBounds } = require('./settings')
 
 const isMac = process.platform === 'darwin';
 
 function createMainWindow () {
+    const bounds = getWindowSettings();
+    
     // Create the browser window.
     const mainWindow = new BrowserWindow({
         title: 'Classify App',
-        width:  1280,
-        height: 800,
+        width:  bounds[0],
+        height: bounds[1],
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         },
         icon: './renderer/images/classify-logo.png'
-    })
+    });
     ipcMain.handle('dialog', (_, options) => dialog.showMessageBox(options));
     ipcMain.handle('setToken', (_, token) => Store.setItem('token', token));
+    
+    mainWindow.on('resized', () => saveBounds(mainWindow.getSize()));
     
     // and load the index.html of the app.
     mainWindow.loadFile(path.join(__dirname, './renderer/sign-in.html'));
